55f3e412e86507c918ad27325ea5f51a13c82e39
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-11-01 15:08:33 -0800

    Implement Initial dummy data seeding schema

---
+++ database-mongo/dummyDump.js
@@ -0,0 +1,86 @@
+// This file created for Amazon S3 link paths for dummy photos served up to seeding script and misc. random data generation
+
+const dummyHeroS3 = [
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/Hero1.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/Hero2.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/Hero3.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/Hero4.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/Hero5.jpg',
+];
+
+const dummyATeamS3 = [
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/AT1.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/AT2.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/AT3.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/AT4.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/AT5.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/AT6.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/AT7.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/AT8.jpg',
+];
+
+const dummyMemLaneS3 = [
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML1.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML2.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML3.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML4.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML5.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML6.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML7.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML8.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML9.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML10.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML11.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML12.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML13.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML14.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML15.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML16.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML17.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML18.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML19.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML20.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML21.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML22.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML23.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML24.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML25.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML26.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML27.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML28.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML29.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML30.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML31.jpg',
+  'https://mightymemvp.s3-us-west-1.amazonaws.com/mvp_ATeam/ML32.jpg',
+];
+
+// =========== RANDOM DESCRIPTION TEXT GENERATOR =======================
+//Initial Response to prompt: 'Hey Ateam, can we do something fun today?
+const randomElement = (array) => {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+};
+
+const intro = [
+  'Hi Susan!', 'Hey girlie!', 'Hola Susana La Mala!', 'Hey Beautiful Sister!', 'Hey Suz!'
+];
+
+const body = [
+  'Yes, of course. Im thinking maybe 3pm today. I can pick you up and we can go to the beach!', 'That is a great idea!! The weather is so nice out, lets do it!',
+  'Ohhhh, we can go see the new blockbuster movie that just came out!',
+];
+
+const ending = [
+  'How does that sound?', 'Sound good?', 'Cant wait! Love you'
+];
+
+const randomDescription = function () {
+  return [randomElement(intro), randomElement(body), randomElement(ending)].join(' ');
+};
+
+module.exports = {
+  dummyHeroS3,
+  dummyATeamS3,
+  dummyMemLaneS3,
+  randomDescription,
+};
--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+const faker = require('faker');
+const dummy = require('./dummyDump.js');
 mongoose.connect('mongodb://localhost/test');
 
 var db = mongoose.connection;
@@ -11,21 +13,139 @@ db.once('open', function() {
   console.log('mongoose connected successfully');
 });
 
-var itemSchema = mongoose.Schema({
-  quantity: Number,
-  description: String
-});
+// ========= CREATE DB SCHEMA =============
+const MightyDev = mongoose.Schema({
+  Hero: {
+    firstName: String,
+    lastName: String,
+    photo: String,
+  },
+  ATeam: {
+    firstName: String,
+    lastName: String,
+    photo: String,
+    email: String,
+    phone: Number,
+    labels: {
+      Children: Boolean,
+      Sibling: Boolean,
+      Parent: Boolean,
+    },
+    messageSend: String,
+    messageReceive: String,
+  },
+  MemoryLane: {
+    photo: String,
+    voices: String,
+    favorite: String
+  },
+  onToday: {
+    date: String,
+    priority: String,
+    social: String,
+    exercise: String,
+    family: String,
+    birthday: String,
+  }
+})
 
-var Item = mongoose.model('Item', itemSchema);
+// =========== CREATE MODEL BASED ON SCHEMA ==============
+const MightyData = mongoose.model('MightyData', MightyDev);
 
-var selectAll = function(callback) {
-  Item.find({}, function(err, items) {
-    if(err) {
-      callback(err, null);
-    } else {
-      callback(null, items);
-    }
-  });
+const heroURL = dummy.dummyHeroS3;
+const ATeamURL = dummy.dummyATeamS3;
+const MemLaneURL = dummy.dummyMemLaneS3;
+
+const initializeData = () => {
+  // Drop Database
+  db.dropDatabase();
+
+   // =========== CREATE SEEDED RECORDS==============
+
+  for (let i = 0; i < 20; i++) {
+    const MightyDataGen = new MightyData({
+      Hero: {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        photo: heroURL[Math.floor(Math.random() * heroURL.length)],
+      },
+      ATeam: {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        photo: ATeamURL[Math.floor(Math.random() * ATeamURL.length)],
+        email: faker.internet.email(),
+        phone: 6501234567,
+        labels: {
+          Children: false,
+          Sibling: true,
+          Parent: false,
+        },
+        messageSend: 'Hi, can we do something fun today?',
+        messageReceive: dummy.randomDescription(),
+      },
+      MemoryLane: {
+        photo: MemLaneURL[Math.floor(Math.random() * MemLaneURL.length)],
+        voices: 'PLACEHOLDER, ADD AUDIO FILE URL',
+        favorite: faker.random.boolean(),
+      },
+      onToday: {
+        date: Date.now(),
+        priority: 'Call Neurologist',
+        social: 'Give Jakey a call!',
+        exercise: 'Walk for 30 minutes with Sylvia',
+        family: 'Make Dinner with Lola',
+        birthday: 'Jakes Birthday today!',
+      }
+
+    });
+
+    MightyDataGen.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Successfully saved seeded record to DB v2!');
+      }
+    });
+  }
 };
 
-module.exports.selectAll = selectAll;
+  // Initialize Data
+  initializeData();
+
+  // ================ RETRIEVE (GET) SAVED DUMY DATA ===============
+  const retrieveData = (callback) => {
+    // initializeData();
+    console.log('reached back end side');
+    MightyData.aggregate([{$sample: { size:20 }}]).exec((err, response) => {
+    if (err) {
+      callback(err);
+    } else {
+      console.log(response);
+      callback(null, response);
+    }})
+  };
+
+  module.exports = {
+    initializeData,
+    retrieveData,
+  };
+
+// =========== LEGACY CODE ==================
+// var itemSchema = mongoose.Schema({
+//   quantity: Number,
+//   description: String
+// });
+
+// var Item = mongoose.model('Item', itemSchema);
+
+// var selectAll = function(callback) {
+//   Item.find({}, function(err, items) {
+//     if(err) {
+//       callback(err, null);
+//     } else {
+//       callback(null, items);
+//     }
+//   });
+// };
+
+// module.exports.selectAll = selectAll;
